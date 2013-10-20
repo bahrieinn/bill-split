b8f146d168bf26ae9dbabb77616cd1dafad076e2
Brian Tong <btong34@gmail.com>
2013-10-19 19:13:52 -0500

    rescue from user not found

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,7 @@
 get '/' do
-  # Look in app/views/index.erb
-  erb :index
+  if current_user
+    erb :profile
+  else
+    erb :index
+  end
 end
--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -3,8 +3,13 @@ before '/users/*' do
   require_login
 end
 
-get '/users/:id' do
-  erb :profile
+get '/users/:user_id' do
+  @user = User.find_by_id(params[:user_id])
+  if @user.nil?
+    redirect "/"
+  else
+    erb :profile
+  end
 end
 
 
