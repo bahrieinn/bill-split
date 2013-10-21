6904c1dcb6a472d5bc9af5c1ff30ce60b2ac4efa
Brian Tong <btong34@gmail.com>
2013-10-20 21:54:31 -0500

    basic seed file to get started

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,22 @@
 require 'faker'
 
-categories = ["Utilities", "Rent", "Groceries", "ComEd"]
+categories = ["Utilities", "Rent", "Groceries"]
+names = ["ComEd", "1600 S Indiana", "Comcast", "Trader Joes", "Costco"]
+
+
+def rand_price
+  num = rand(10..200).to_s
+  return num + ".00"
+end
+
+brian = User.create(:first_name => "Brian",
+            :last_name => "Tong",
+            :email => "btong34@gmail.com",
+            :password => "test")
+
 5.times do
+  brian.credited_expenses.create(:category => categories.sample,
+                                 :name => names.sample,
+                                 :total => rand_price)
+end
+
